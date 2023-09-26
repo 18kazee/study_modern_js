bf31ca57179ae8135bf113565c4cbc9392920c8a
18kazee <18kazee2415@gmail.com>
2023-09-26 17:47:20 +0900

    numが数値の時はtoLocaleString()を使用して三桁括りに、そうでない場合はメッセージ表示

--- js/index.js
+++ js/index.js
@@ -192,6 +192,12 @@
  * 三項演算子
  */
 
-ある条件 ? 条件がtrueの時 : 条件がfalseの時
-const val1 = 1 < 0 ? 'trueです' : 'falseです';
-console.log(val1);
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+const num = "1300";
+// console.log(num.toLocaleString());
+
+const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください。';
+console.log(formattedNum);
