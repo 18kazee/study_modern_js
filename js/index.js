18bdca1bdbc2c36b919fabf53926bbdb6f619d51
18kazee <18kazee2415@gmail.com>
2023-09-26 16:40:37 +0900

    配列のコピーと結合について

--- js/index.js
+++ js/index.js
@@ -134,8 +134,25 @@
 // sumFunc(...arr1);
 
 //まとめる
-const arr2 = [1, 2, 3, 4, 5];
-const[num1, num2, ...arr3] = arr2;
-console.log(num1);
-console.log(num2);
-console.log(arr3);
+// const arr2 = [1, 2, 3, 4, 5];
+// const[num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+//配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+const arr6 = [...arr4];
+arr6[0] = 100;
+console.log(arr6);
+console.log(arr4);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// const arr8 = arr4;
+// arr8[0] = 100;
+// console.log(arr8);
+// console.log(arr4);
