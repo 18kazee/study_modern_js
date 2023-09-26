23778934bb7ada4bfa7cfc3ed0b2995c3722cdc3
18kazee <18kazee2415@gmail.com>
2023-09-26 17:19:32 +0900

    mapにindex情報を持たせて取得する(引数2番目に入れておくと取れる)

--- js/index.js
+++ js/index.js
@@ -161,20 +161,20 @@
  * mapやfilterを使った配列の処理
  */
 
-// const nameArr = ["田中", "山田", "じゃけえ"];
+const nameArr = ["田中", "山田", "じゃけえ"];
 // for(let index = 0; index < nameArr.length; index++) {
-//   console.log(nameArr[index]);
-// }
+//   console.log(`${index + 1}番目は${nameArr[index]}です。`);
+//   }
 
 // const nameArr2 = nameArr.map((name) => {
 //   return name;
 // })
 // console.log(nameArr2);
 
-// nameArr.map((name) => console.log(name));
+nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です。`));
 
-const numArr = [1, 2, 3, 4, 5];
-const newNumArr = numArr.filter((num) => {
-  return num % 2 === 0;
-});
-console.log(newNumArr);
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 0;
+// });
+// console.log(newNumArr);
